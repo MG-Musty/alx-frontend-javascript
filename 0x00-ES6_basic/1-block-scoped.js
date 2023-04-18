25c35684694fb44a0581b33d1c915293cd112374
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-04-18 13:18:50 -0700

    block scoped task done

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+var task = false;
+var task2 = true;
+
+export default function taskBlock() {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+
+  return [task, task2];
+}
