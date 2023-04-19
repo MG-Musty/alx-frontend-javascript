886934781997549d4b52b37e3fba0c41336c06d2
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-04-19 13:55:36 -0700

    get budget year

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,14 +5,9 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-	income: '2100',
-	gdp: '5200',
-	capita: '1090',
-  };
-
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
+    [`income-${getCurrentYear()}`] = income;
+    [`gdp-${getCurrentYear()}`] = gdp;
+    [`capita-${getCurrentYear()}`] = capita;
 
   return budget;
 }
