a0d3a2d445dd1532032b0f835ef3e065f540b7c0
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-04-19 12:28:38 -0700

    rest parameter

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,8 @@
 export default function returnHowManyArguments(...args) {
   let sum = 0;
         
-  for(let i of input){
-      sum+=i;
+  for(let i of input) {
+    sum += i;
   }
   return sum;
 }
