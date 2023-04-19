2f8ca0f36daa10cd814d67abfca73f292bba3fc9
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-04-19 12:16:39 -0700

    rest parameter

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,4 +1,6 @@
 export default function returnHowManyArguments("one", "two", 3, "4th") {
-  return one + two + 3 + 4th;
+    return one + two + 3 + 4th;
 }
+console.log(returnHowManyArguments("one"));
+console.log(returnHowManyArguments("one", "two", 3, "4th"));
 returnHowManyArguments("one", "two", 3, "4th");
