39fd7c2fcedf252e2078de3aebcf9521ab91e32b
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-04-19 14:12:45 -0700

    get full budget

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,10 +3,7 @@ import getBudgetObject from './7-getBudgetObject.js';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-        income: '20',
-	gdp: '50',
-	capita: '10',
-
+    ...budget,
     getIncomeInDollars(income) {
       return `$${income}`;
     },
