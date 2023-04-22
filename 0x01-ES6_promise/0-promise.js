f3af2caebd922c97faf05c268d2eca15e67919ec
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-04-22 12:27:56 -0700

    return promise func

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,6 @@
 function getResponseFromAPI(){
   
   return new Promise((resolve, reject) => {
-  setTimeout(() => {
-  resolved('resolved');
-    }, 2000);
   });
 }
 export default getResponseFromAPI;
