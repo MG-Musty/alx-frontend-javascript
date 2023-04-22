5e266a88afa75cb7031c11c5de0c1f2d0a98f961
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-04-22 12:30:06 -0700

    return promise func

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,7 @@
-function getResponseFromAPI(){
-  
+function getResponseFromAPI() {
+  // eslint-disable-next-line
   return new Promise((resolve, reject) => {
   });
 }
+
 export default getResponseFromAPI;
