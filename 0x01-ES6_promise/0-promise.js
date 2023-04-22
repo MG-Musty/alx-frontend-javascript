ce158f32cdddf8f47ce9250a58bc227c38bcaf04
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-04-22 12:13:18 -0700

    return a promise resolved

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI(){
+  return new Promise((resolve, reject) => {
+  settTimeout(() => {
+  resolved('resolved');
+    }, 2000);
+  });
+
+  async function getResponseFromAPI()
